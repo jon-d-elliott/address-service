d68d995dae6875adcbdba609794aa0e8d7a88e6d
Jonathan <jonathan.d.elliott@outlook.com>
2024-02-23 10:08:08 +0000

    updated to add support for getting a specific address

--- main.go
+++ main.go
@@ -25,32 +25,6 @@ type getCustomerAddressesResponse struct {
 	PostCode string `json:"postCode"`
 }
 
-
-type creatAddressRequest struct {
-	CustomerId string `json:"customerId"`
-	Name string `json:"name,omitempty"`
-	Number string `json:"number,omitempty"`
-	StreetName string `json:"streetName"`
-	Locality string `json:"locality"`
-	PostTown string `json:"postTown"`
-	PostCode string `json:"postCode"`
-}
-
-type createAddressResponse struct {
-	AddressId string `json:"addressId"`
-}
-
-type updateAddressRequest struct {
-	Name string `json:"name,omitempty"`
-	Number string `json:"number,omitempty"`
-	StreetName string `json:"streetName"`
-	Locality string `json:"locality"`
-	PostTown string `json:"postTown"`
-	PostCode string `json:"postCode"`
-}
-
-
-
 var addressMap = map[string]map[string]address { 
 	"1": {
 		"1": {
@@ -72,34 +46,56 @@ var addressMap = map[string]map[string]address {
 }
 
 func getCustomerAddresses(c *gin.Context) {
-	customerId := c.Param("customerId")
-	var custAddresses []getCustomerAddressesResponse
-	for k,v  := range addressMap {
-		if k == customerId {
-			for k1,v1 := range v {
-				address := getCustomerAddressesResponse{
-					AddressId: k1,
-					Name: v1.Name,
-					Number: v1.Number,
-					StreetName: v1.StreetName,
-					Locality: v1.Locality,
-					PostTown: v1.PostTown,
-					PostCode: v1.PostCode,
-					}
-					custAddresses = append(custAddresses, address)
+	customerId := c.Query("customerId")
+
+	if customerId == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "customer id not supplied"})
+	}else {
+		var custAddresses []getCustomerAddressesResponse
+		for k,v  := range addressMap {
+			if k == customerId {
+				for k1,v1 := range v {
+					address := getCustomerAddressesResponse{
+						AddressId: k1,
+						Name: v1.Name,
+						Number: v1.Number,
+						StreetName: v1.StreetName,
+						Locality: v1.Locality,
+						PostTown: v1.PostTown,
+						PostCode: v1.PostCode,
+						}
+						custAddresses = append(custAddresses, address)
+				}
 			}
 		}
+		if len(custAddresses) > 0 {
+			c.IndentedJSON(http.StatusOK, custAddresses)
+		}else {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "no addresses found for customer : " + customerId})
+		}
 	}
-	if len(custAddresses) > 0 {
-		c.IndentedJSON(http.StatusOK, custAddresses)
+}
+
+func getAddressById(c *gin.Context) {
+	customerId := c.Query("customerId")
+	if customerId == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "customer id not supplied"})
 	}else {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "no addresses found for customer : " + customerId})
+		addressId := c.Param("addressId")
+		theAddress,exists := addressMap[customerId][addressId]
+		if !exists {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Address not found"})
+		}else {
+			c.IndentedJSON(http.StatusOK,theAddress)
+		}
 	}
 }
 
 
+
 func main() {
 	router := gin.Default()
-	router.GET("/addresses/customers/:customerId", getCustomerAddresses)
+	router.GET("/addresses", getCustomerAddresses)
+	router.GET("/addresses/addressId/:addressId", getAddressById)
 	router.Run("localhost:8080")
 }
