b8ba69868456e9ae9ac10484575b280ce18c8651
Jonathan <jonathan.d.elliott@outlook.com>
2024-02-23 13:11:30 +0000

    refactored types

---
+++ types.go
@@ -0,0 +1,30 @@
+package main
+
+type address struct {
+	Name       string `json:"name,omitempty"`
+	Number     string `json:"number,omitempty"`
+	StreetName string `json:"streetName"`
+	Locality   string `json:"locality"`
+	PostTown   string `json:"postTown"`
+	PostCode   string `json:"postCode"`
+}
+
+type createAddressRequest struct {
+	CustomerId string `json:"customerId`
+	Name       string `json:"name,omitempty"`
+	Number     string `json:"number,omitempty"`
+	StreetName string `json:"streetName"`
+	Locality   string `json:"locality"`
+	PostTown   string `json:"postTown"`
+	PostCode   string `json:"postCode"`
+}
+
+type getCustomerAddressesResponse struct {
+	AddressId  string `json:"addressId"`
+	Name       string `json:"name,omitempty"`
+	Number     string `json:"number,omitempty"`
+	StreetName string `json:"streetName"`
+	Locality   string `json:"locality"`
+	PostTown   string `json:"postTown"`
+	PostCode   string `json:"postCode"`
+}
